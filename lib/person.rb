9c48e0003de18247ea486c200269f76b5c50af6d
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-18 16:09:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,10 @@
 class Person
   def name=(persons_name)
-    @persons_name = persons_name
+    @name = persons_name
   end
   
   def name
-    @persons_name
+    @name
   end
   
   def jobs=(job)
