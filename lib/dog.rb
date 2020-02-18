f1d65618f70ab4b8a9f312591e3160e44a2cb6ad
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-18 16:05:57 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog.new 
+  def name=(dogs_name)
+    @dogs_name = dogs_name
+  end
+  
+  def name
+    @dogs_name
+  end
+  
+  def breed=(dogs_breed)
+    @dogs_breed = dogs_breed
+  end
+  
+  def breed
+    @dogs_breed
+  end
+end
